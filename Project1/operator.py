97faf54909513367905e499bd9e77d4de847bf1d
Estifanos99 <estifaynalem99@gmail.com>
2023-05-14 16:22:14 -0400

    Renameing minilik test file

---
+++ Project1/operator.py
@@ -0,0 +1,23 @@
+# # Arithmetic Operator
+# num1=12
+# num2=23
+# num3=num1+num2  # num1,num2,num3==>Operands
+ 
+# print(num3)
+# a=19
+# b=9
+# c=a%b
+# print(c)
+
+# number1=10
+# number1+=1
+# print(number1)
+# num1=40
+# num2=30
+# print(num1==num2)
+# Estif=25
+# Herma=24
+# print(Estif!=Herma)
+# logical and or not
+name="Estifanos"
+print("s" not in name)
